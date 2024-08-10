6d73efd52d2358de5d414bf3729545956b001d38
Austin M. Weber <133900499+weber1158@users.noreply.github.com>
2024-08-10 15:43:37 -0400

    Version 3.0

---
+++ ByrdDIT.m
@@ -0,0 +1,8 @@
+%% Open the Byrd Data Import Tool (ByrdDIT) App
+% The legacy apps were called ByrdApp instead of ByrdDIT
+% and so this function makes it possible for either command
+% to execute the same task.
+
+ByrdApp
+
+% Copyright (C) 2024 Austin M. Weber
